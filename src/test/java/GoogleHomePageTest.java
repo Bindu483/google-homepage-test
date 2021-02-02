99624fb7c50ca6168a330c4e07782fc817c023ba
Bindu483 <binduvanaja1998@gmail.com>
2021-02-02 20:15:19 +0530

    adding testsetup and testteardown methods

--- src/test/java/GoogleHomePageTest.java
+++ src/test/java/GoogleHomePageTest.java
@@ -2,27 +2,47 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class GoogleHomePageTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void TestSetUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Desktop\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.manage().window().maximize();
+
+    }
+
+    @AfterMethod
+    public void TestTearDown() {
+        driver.quit();
+    }
+
     @Test
     public void GoogleHomePageTitleTest() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Desktop\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+
         String actualPageTitle = driver.getTitle();
         Assert.assertEquals("Google", actualPageTitle);
-        driver.quit();
 
     }
+
     @Test
-    public void GmailLinkTest(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\LENOVO\\Desktop\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        boolean isLinkDisplayed=driver.findElement(By.linkText("Gmail")).isDisplayed();
-        Assert.assertEquals(true,isLinkDisplayed);
-        driver.quit();
+    public void GmailLinkTest() {
+        boolean isLinkDisplayed = driver.findElement(By.linkText("Gmail")).isDisplayed();
+        Assert.assertEquals(true, isLinkDisplayed);
 
     }
+
+    @Test
+    public void ImageLinkTest() {
+        boolean isLinkDisplayed = driver.findElement(By.linkText("Images")).isDisplayed();
+        Assert.assertTrue(isLinkDisplayed);
+    }
+
+
 }
